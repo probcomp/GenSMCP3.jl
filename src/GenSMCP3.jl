c794dfbb791431d428ad464ecda349a3e8eb98a8
George Matheos <georgematheos@gmail.com>
2023-06-29 20:00:56 +0000

    add stratified SMCP3 update

--- src/GenSMCP3.jl
+++ src/GenSMCP3.jl
@@ -32,6 +32,40 @@ function GenParticleFilters.pf_update!(
     GenParticleFilters.update_refs!(state)
 end
 
+#=
+Stratified SMCP3 update.
+
+The `strata` argument is an iterator of `Gen.ChoiceMap`s, one for each stratum.
+Each choicemap constrains addresses in the generative model.
+The SMCP3 K kernel will be given each stratum as an additional (final) argument.
+(So the SMCP3 update passed into this should have the K_args be 1 element too short.)
+=#
+function GenParticleFilters.pf_update!(
+    state::Gen.ParticleFilterState,
+    new_args::Tuple,
+    argdiffs::Tuple,
+    observations::Gen.ChoiceMap,
+    strata,
+    update::SMCP3Update
+)
+    n_particles = length(state.traces)
+    n_strata = length(strata)
+    @assert n_particles >= n_strata
+    GenParticleFilters.stratified_map!(n_particles, strata) do i, stratum
+        state.new_traces[i], log_weight_update = GenTraceKernelDSL.run_smcp3_step(
+            state.traces[i], new_args, argdiffs,
+            merge(observations, stratum),
+            update.K, update.L,
+            (update.K_args..., stratum),
+            update.L_args;
+            check_are_inverses=update.check_are_inverses
+        )
+        state.log_weights[i] += log_weight_update + log(n_strata)
+    end
+    GenParticleFilters.update_refs!(state)
+    return state
+end
+
 function GenParticleFilters.pf_initialize(
     model::Gen.GenerativeFunction{T,U}, model_args::Tuple,
     observations::Gen.ChoiceMap, n_particles::Int, update::SMCP3Update;
